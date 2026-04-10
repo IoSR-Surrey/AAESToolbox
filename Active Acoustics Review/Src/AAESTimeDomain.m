beb81850daae5a3686717445ef0f0f396b9f4769
Will Cassidy <60139374+willcassidy00454@users.noreply.github.com>
2026-04-10 14:16:33 +0100

    Add time domain simulation for time-varying reverberator comparison

---
+++ Active Acoustics Review/Src/AAESTimeDomain.m
@@ -0,0 +1,192 @@
+classdef AAESTimeDomain < handle
+
+    % Properties
+    properties
+        % time-frequency
+        fs
+        nfft
+        blockSize
+        % layout
+        numMics
+        numLds
+        % RIRs
+        H_SR
+        H_SM
+        H_LR
+        H_LM
+        % DSP
+        FDN
+        generalGain
+        % Storages
+        mics_storage
+        receivers_storage
+    end
+    
+    methods
+        
+        % Constructor
+        function obj = AAESTimeDomain(config, RIRs, FDN_params)
+
+            % Processing
+            obj.fs = config.fs;
+            obj.nfft = RIRs.length;
+            obj.blockSize = config.blockSize;
+            % Layout
+            obj.numMics = config.numMicrophones;
+            obj.numLds = config.numLoudspeakers;
+            % RIRs
+            obj.H_SR = RIRs.H_SR;
+            obj.H_SM = RIRs.H_SM;
+            obj.H_LR = RIRs.H_LR;
+            obj.H_LM = RIRs.H_LM;
+            % DSP
+            obj.create_FDN(FDN_params);
+
+            if (config.gainIsRelativeToGBI)
+                obj.generalGain = db2mag(config.gain)*obj.estimateGBI();
+            else
+                % % % % use this to allow the eigenvalues to be plotted if
+                % desired
+                % obj.estimateGBI();
+
+                obj.generalGain = db2mag(config.gain);
+            end
+
+            % Storages
+            obj.mics_storage = zeros(obj.nfft, obj.numMics);
+            obj.receivers_storage = zeros(obj.nfft,1);
+
+        end
+
+        % Generate FDN
+        function create_FDN(obj, params)
+            % Reverberation time
+            obj.FDN.RT = max(params.RT_DC, params.RT_NY);
+
+            % Input gains
+            B = convert2zFilter(params.inputGains);
+            obj.FDN.InputGains = dfiltMatrix(B);
+            % Delay lines
+            obj.FDN.DelayFilters = feedbackDelay(obj.blockSize, params.delays);
+            % Absorption filters
+            [absorption.b,absorption.a] = onePoleAbsorption(params.RT_DC, params.RT_NY, params.delays, obj.fs);
+            A = zTF(absorption.b, absorption.a,'isDiagonal', true);
+            obj.FDN.absorptionFilters = dfiltMatrix(A); 
+            % Feedback matrix
+            F = convert2zFilter(params.feedbackMatrix);
+            obj.FDN.FeedbackMatrix = dfiltMatrix(F);
+            obj.FDN.TVMatrix = timeVaryingMatrix(params.order, params.modulationFrequency, params.modulationAmplitude, obj.fs, params.spread);
+            % Output gains
+            C = convert2zFilter(params.outputGains);
+            obj.FDN.OutputGains = dfiltMatrix(C);
+            % Direct path
+            D = convert2zFilter(params.directGains);
+            obj.FDN.DirectGains = dfiltMatrix(D);
+
+        end
+
+        % Gain before instability estimation
+        function GBI = estimateGBI(obj)
+            FDN_irs = obj.computeFDNirs();
+            open_loop_TFs = matrix_product( fft(obj.H_LM, obj.nfft, 1), fft(FDN_irs, obj.nfft, 1) );
+
+            % PlotEigenvalues(open_loop_TFs, obj.fs, true);
+
+            eigenvalues = zeros(obj.nfft, obj.numMics);
+            for f = 1:obj.nfft
+                eigenvalues(f,:) = eig(permute(open_loop_TFs(f,:,:), [2,3,1]));
+            end
+            GBI = 1/max(abs(eigenvalues),[],'all');
+            disp(strcat(['The current GBI is: ', sprintf('%0.1f',(mag2db(GBI)))]));
+        end
+
+        function FDN_irs = computeFDNirs(obj)
+
+            % Define length of the FDN irs based on the FDN RT
+            sigLength = obj.FDN.RT * obj.fs;
+            
+            % Allocate memory
+            FDN_irs = zeros(sigLength, obj.numLds, obj.numMics);
+
+            % Iterate over FDN inputs
+            for i = obj.numMics
+                
+                % Define input signal (Impulse at a single channel)
+                inputSignal = zeros(sigLength, obj.numMics);
+                inputSignal(1,i) = 1;
+
+                % Block processing
+                numBlocks = floor(sigLength / obj.blockSize);
+                for block = 1:numBlocks
+                    block_index = (block-1)*obj.blockSize + (1:obj.blockSize);
+                    FDN_irs(block_index,:,i) = obj.FDN_step(inputSignal(block_index,:));
+                end
+            end
+
+            obj.FDN.DelayFilters.values(:) = 0;
+
+        end
+
+        % Process signal
+        function output = process(obj, input)
+
+            % Input block
+            assert(size(input,1) == obj.blockSize);
+            % Microphone input
+            source_to_mics = real(ifft( matrix_product( fft(obj.H_SM, obj.nfft, 1), fft(input, obj.nfft, 1) ), obj.nfft, 1));
+            mics_signals = source_to_mics(1:obj.blockSize,:) + obj.mics_storage(1:obj.blockSize,:);
+            % FDN
+            FDN_input = mics_signals;
+            FDN_output = obj.FDN_step(FDN_input);
+            % Loudspeaker output
+            ls_signals = obj.generalGain * FDN_output;
+            % Feedback
+            ls_to_mics = real(ifft( matrix_product( fft(obj.H_LM, obj.nfft, 1), fft(ls_signals, obj.nfft, 1) ), obj.nfft, 1));
+            % Receiver signals
+            ls_to_receivers = real(ifft( matrix_product( fft(obj.H_LR, obj.nfft, 1), fft(ls_signals, obj.nfft, 1) ), obj.nfft, 1));
+            source_to_receivers = real(ifft( matrix_product( fft(obj.H_SR, obj.nfft, 1), fft(input, obj.nfft, 1) ), obj.nfft, 1));
+            receivers_signal = ls_to_receivers + source_to_receivers;
+            % Output block
+            output = receivers_signal(1:obj.blockSize) + obj.receivers_storage(1:obj.blockSize);
+            % Store for next block iterations
+            obj.update_storage("mics_storage", source_to_mics+ls_to_mics);
+            obj.update_storage("receivers_storage", receivers_signal);
+
+        end
+
+        % FDN iteration step
+        function output = FDN_step(obj, input)
+                
+            % Delays 
+            delayOutput = obj.FDN.DelayFilters.getValues(obj.blockSize);
+            % Absorption
+            absorptionOutput = obj.FDN.absorptionFilters.filter(delayOutput); 
+            % Feedback matrix
+            feedback = obj.FDN.FeedbackMatrix.filter(absorptionOutput);
+            if ~isempty(obj.FDN.TVMatrix)
+                feedback = obj.FDN.TVMatrix.filter(feedback);
+            end
+            % Output
+            output = obj.FDN.OutputGains.filter(absorptionOutput) + obj.FDN.DirectGains.filter(input);
+
+            % Prepare next iteration
+            delayLineInput = obj.FDN.InputGains.filter(input) + feedback;
+            obj.FDN.DelayFilters.setValues(delayLineInput);
+            obj.FDN.DelayFilters.next(obj.blockSize);
+            
+        end
+
+        % Store signal blocks
+        function update_storage(obj, storage, newData)
+
+            % Add new data
+            obj.(storage) = obj.(storage) + newData;
+            % Shift left
+            obj.(storage) = circshift(obj.(storage), -obj.blockSize, 1);
+            % Clean end
+            obj.(storage)(end-(obj.blockSize-1):end, :) = 0;
+
+        end
+
+    end
+end
---
+++ Active Acoustics Review/Src/SimulateAAESTimeDomain.m
@@ -0,0 +1,146 @@
+%% Housekeeping
+clc;
+
+% Path to FDN toolbox
+addpath(genpath('../GitHub/fdnToolbox'));
+
+%% High-level params
+rir_read_dir = "Active Acoustics Review/Generated AAES RIRs/Room Condition 2/";
+output_dir = "Active Acoustics Review/AAES Receiver RIRs/";
+output_filename = "AAES_Inline_TI_-2dB.wav";
+is_time_varying = false;
+config.gain = -2; % dB
+config.gainIsRelativeToGBI = true;
+rt_ratio = 1.0;
+impulse_length_sec = 4;
+bit_depth = 32;
+
+%% Configuration data
+
+% Time parameters
+config.fs = 48000; % [Hz]
+config.blockSize = 256;
+
+%% Input Signals
+
+% Unit impulse:
+anechoic_signal = zeros(impulse_length_sec * config.fs, 1);
+anechoic_signal(1) = 1;
+fs_an = config.fs;
+
+if fs_an ~= config.fs
+    anechoic_signal = resample(anechoic_signal, config.fs, fs_an);
+end
+inputSignal = anechoic_signal;
+sigLength = size(inputSignal,1);
+
+%%
+
+% Layout
+config.numMicrophones = 4;
+config.numLoudspeakers = 16;
+
+%% Load room impulse responses (only 1 input source and 1 audience position)
+
+filename = rir_read_dir + "E_R1_S1.wav";
+[RIRs.H_SR, fs_rirs] = audioread(filename);                                         % [time, 1, 1]
+assert(fs_rirs == config.fs);
+RIRs.length = size(RIRs.H_SR, 1);
+
+RIRs.H_SM = zeros(RIRs.length, config.numMicrophones, 1);                           % [time, n_mics, 1]
+RIRs.H_LR = zeros(RIRs.length, 1, config.numLoudspeakers);                          % [time, 1, n_speakers]
+RIRs.H_LM = zeros(RIRs.length, config.numMicrophones, config.numLoudspeakers);      % [time, n_mics, n_speakers]
+
+for rec_index = 1:config.numMicrophones
+    filename = rir_read_dir + "G_R" + rec_index + "_S1.wav";
+    ir = audioread(filename);
+    if length(ir) < RIRs.length
+        ir = cat(1, ir, zeros(RIRs.length - length(ir), 1));
+    end
+    RIRs.H_SM(:,rec_index,1) = ir(1:RIRs.length);
+end
+
+for src_index = 1:config.numLoudspeakers
+    filename = rir_read_dir + "F_R1_S" + src_index + ".wav";
+    ir = audioread(filename);
+    if length(ir) < RIRs.length
+        ir = cat(1, ir, zeros(RIRs.length - length(ir), 1));
+    end
+    RIRs.H_LR(:,1,src_index) = ir(1:RIRs.length);
+end
+
+for rec_index = 1:config.numMicrophones
+    for src_index = 1:config.numLoudspeakers
+        filename = rir_read_dir + "H_R" + rec_index + "_S" + src_index + ".wav";
+        ir = audioread(filename);
+        if length(ir) < RIRs.length
+            ir = cat(1, ir, zeros(RIRs.length - length(ir), 1));
+        end
+        RIRs.H_LM(:,rec_index,src_index) = ir(1:RIRs.length);
+    end
+end
+
+RIRs.length = RIRs.length;
+
+%% Define FDN parameters and general gain
+
+rng(1);
+
+% FDN order
+FDN.order = 32;
+
+% Gains
+FDN.inputGains = orth(randn(FDN.order, config.numMicrophones));
+FDN.outputGains = orth(randn(config.numLoudspeakers, FDN.order)')';
+FDN.directGains = zeros(config.numLoudspeakers,config.numMicrophones);
+
+% Delay lines
+FDN.delays = randi([1200,2500], [1,FDN.order]);
+
+% Feedback matrix
+FDN.feedbackMatrix = randomOrthogonal(FDN.order);
+
+% Absoption filters
+FDN.RT_DC = 0.868 * rt_ratio;                % [seconds]
+FDN.RT_NY = FDN.RT_DC / 2;                % [seconds]
+
+% Time Variation
+if (is_time_varying)
+    FDN.modulationFrequency = 1.0;  % Hz
+    FDN.modulationAmplitude = 1.0;
+    FDN.spread = 20.0;
+else
+    FDN.modulationFrequency = 0.0;  % Hz
+    FDN.modulationAmplitude = 0.0;
+    FDN.spread = 0.0;
+end
+
+% The class computes the GBI and then sets the general gain to mu = db2mag(GBI + GBI-offset)
+
+%% Active Acoustic Enhancement System
+
+aaes = AAESTimeDomain(config, RIRs, FDN);
+
+%% Process input signal
+
+% Allocate memory
+outputSignal = zeros(sigLength,1);
+
+% Number of processing blocks
+numBlocks = floor(sigLength / config.blockSize);
+
+% Block-wise processing
+for block = 1:numBlocks
+    
+    % Time indeces
+    block_index = (block-1)*config.blockSize + (1:config.blockSize);
+    % Processing function
+    outputSignal(block_index) = aaes.process(inputSignal(block_index));
+
+end
+
+%% Output results
+
+outputSignal = outputSignal / max(abs(outputSignal));
+
+audiowrite(output_dir + output_filename, outputSignal, config.fs, "BitsPerSample", bit_depth);
